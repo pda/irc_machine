4283969a13c08153c9f70180dbf7929288847e32
Paul Annesley <paul@annesley.cc>
2011-06-05 17:16:07 +1000

    Tweak to Reloader loop.

--- lib/irc_machine/observer/reloader.rb
+++ lib/irc_machine/observer/reloader.rb
@@ -1,9 +1,10 @@
 class IrcMachine::Observer::Reloader < IrcMachine::Observer::Base
   def receive_line(line)
     if line =~ /^:\S+ PRIVMSG #irc_machine :reload$/
-      observers = %w{ base die hello ping reloader verbose }
-      puts "reloading " << observers.join(', ')
-      observers.each { |name| load "irc_machine/observer/#{name}.rb" }
+      %w{ base die hello ping reloader verbose }.each do |name|
+        puts "reloading observer: #{name}"
+        load "irc_machine/observer/#{name}.rb"
+      end
     end
   end
 end
