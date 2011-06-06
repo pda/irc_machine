aac8ee5994074ab64b1b9f4bd2f2252914efdea6
Paul Annesley <paul@annesley.cc>
2011-06-07 01:26:06 +1000

    Plugin::Hello responds in any channel, not just #irc_machine.

--- lib/irc_machine/plugin/hello.rb
+++ lib/irc_machine/plugin/hello.rb
@@ -1,5 +1,7 @@
 class IrcMachine::Plugin::Hello < IrcMachine::Plugin::Base
   def receive_line(line)
-    session.msg "#irc_machine", "meh" if line =~ /^:\S+ PRIVMSG #irc_machine :hello$/
+    if line =~ /^:\S+ PRIVMSG (#+\S+) :hello$/
+      session.msg $1, "world"
+    end
   end
 end
