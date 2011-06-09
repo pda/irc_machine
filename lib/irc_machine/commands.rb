d815d44e7a17cc8e141af27def7802546bbd294c
Paul Annesley <paul@annesley.cc>
2011-06-09 22:01:07 +1000

    Expanded one-line methods to multi-line in IrcMachine::Commands.

--- lib/irc_machine/commands.rb
+++ lib/irc_machine/commands.rb
@@ -6,12 +6,29 @@ module IrcMachine
       irc_connection.send_data "#{raw}\r\n"
     end
 
-    def user(user, name); raw "USER #{user} 8 * :#{name}"; end
-    def nick(nick); raw "NICK #{nick}"; end
-    def join(channel); raw "JOIN #{channel}"; end
-    def part(channel); raw "PART #{channel}"; end
-    def quit(reason); raw "QUIT :#{reason}"; end
-    def msg(to, text); raw "PRIVMSG #{to} :#{text}"; end
+    def user(user, name)
+      raw "USER #{user} 8 * :#{name}"
+    end
+
+    def nick(nick)
+      raw "NICK #{nick}"
+    end
+
+    def join(channel)
+      raw "JOIN #{channel}"
+    end
+
+    def part(channel)
+      raw "PART #{channel}"
+    end
+
+    def quit(reason)
+      raw "QUIT :#{reason}"
+    end
+
+    def msg(to, text)
+      raw "PRIVMSG #{to} :#{text}"
+    end
 
   end
 end
