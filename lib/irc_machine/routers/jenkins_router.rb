859373506ade5b6f7ade2054000a983ce5d90caa
Rich Healey <richard.healey@99designs.com>
2012-04-23 15:03:36 +1000

    Build a router class for wrapping Jenkins Notifier requests

---
+++ lib/irc_machine/routers/jenkins_router.rb
@@ -0,0 +1,58 @@
+# This is not a valid plugin, it can't be directly attached to an endpoint.
+#
+# I just couldn't work out where else this code belonged.
+#
+# Usage:
+#  def initialize_jenkins_notifier
+#    @notifier = JenkinsNotifier.new(@builds) do |endpoint|
+#      endpoint.on :success do |commit, build|#{{{ Success
+#        notify format_msg(commit, build)
+#      end #}}}
+#
+#      endpoint.on :failure do |commit, build| #{{{ Failure
+#        notify format_msg(commit, build)
+#        notify "Jenkins output available at #{build.full_url}"
+#      end #}}}
+#    end
+#  end
+#
+#  With subsequent calls to
+#  @notifier.process([body of jenkins notification])
+class IrcMachine::Routers::JenkinsRouter
+  attr_reader :builds, :triggers
+  def initialize(builds)
+    @builds = builds
+    @triggers = {}
+    if block_given?
+      yield self
+    end
+  end
+
+  def on(phase, status=:any, &block)
+    triggers[phase] ||= {}
+    triggers[phase][status] = block
+  end
+
+  def process(message)
+    build = ::IrcMachine::Models::JenkinsNotification.new(message)
+
+    if match = triggers[build.phase.downcase.to_sym]
+      if commit = @builds[build.parameters.ID.to_s]
+        if block = match[build.status.downcase.to_sym] rescue nil
+          block.call(commit, build)
+        elsif block = match[:any]
+          block.call(commit, build)
+        end
+      else
+        if block = triggers[:unknown][:any]
+          block.call(build)
+        end
+      end
+    end
+  end
+
+  def endpoint
+    lambda { |request, match| process(request.body.read) }
+  end
+end
+
