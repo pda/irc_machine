9532855e97cc331eff6ebb24b272f7b0a7c22c4b
Rich Healey <richard.healey@99designs.com>
2012-06-07 12:30:10 +1000

    Fix references to hash elements

--- lib/irc_machine/plugin/imageme.rb
+++ lib/irc_machine/plugin/imageme.rb
@@ -13,10 +13,10 @@ class IrcMachine::Plugin::ImageMe < IrcMachine::Plugin::Base
     uri.query = URI.encode_www_form({ 'v' => 1.0, 'rsz' => 8, 'safe' => 'active', 'q' => query })
     response = Net::HTTP.get(uri)
 
-    images = JSON.parse(response).responseData.results
+    images = JSON.parse(response)['responseData']['results']
     if images.length > 0
       image = images.sample
-      "#{image.unescapedUrl}"
+      "#{image['unescapedUrl']}"
     end
   end
 
