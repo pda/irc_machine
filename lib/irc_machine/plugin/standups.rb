3d78eb3b4673713574c823e6cf2fb90f07661634
richo <richo@psych0tik.net>
2012-06-19 17:32:56 +1000

    Fix some precendence and parsing issues

--- lib/irc_machine/plugin/standups.rb
+++ lib/irc_machine/plugin/standups.rb
@@ -13,17 +13,22 @@ class IrcMachine::Plugin::Standups < IrcMachine::Plugin::Base
   end
 
   def receive_line(line)
-    if line =~ /^:(\S+)!\S+ PRIVMSG #{config.channel} :(.*)$/
-      tasks[$1] = $2
-    elsif line =~ /^:(\S+)!\S+ PRIVMSG (#\S+) :what(?:'s| is) (\S+) (?:up to|working on)\?/
+    if line =~ /^:(\S+)!\S+ PRIVMSG (#\S+) :#{session.state.nick}(?::)? what(?:'s| is) (\S+) (?:up to|working on)\?/
       nick, channel, target = $1, $2, $3
       if target =~ /every(one|body)/
         @tasks.each do |person, data|
           session.msg nick, "#{person} is doing: #{data}"
         end
       else
-        session.msg channel, "#{nick}: person is doing #{data}"
+        if tasks.include? target
+          session.msg channel, "#{nick}: #{target} is doing #{tasks[target]}"
+        else
+          session.msg channel, "#{nick}: I'm not sure what #{target} has planned"
+        end
       end
+    elsif line =~ /^:(\S+)!\S+ PRIVMSG #{config.channel} :(.*)$/
+      puts "tasks[#{$1}] = #{$2}"
+      tasks[$1] = $2
     end
   end
 
