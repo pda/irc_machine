44e4876ef38d1c5b6b32af3835ced4779c3542a3
Rich Healey <richard.healey@99designs.com>
2012-06-28 11:44:09 +1000

    Show status instead of Object ref

--- lib/irc_machine/plugin/github_jenkins.rb
+++ lib/irc_machine/plugin/github_jenkins.rb
@@ -79,7 +79,7 @@ class IrcMachine::Plugin::GithubJenkins < IrcMachine::Plugin::Base
   end
 
   def all_builds_status(request, match)
-    ok (@builds.map {|k, v| "#{k} => #{v}" }.join("\r\n"))
+    ok (@builds.map {|k, v| "#{k} => #{v.status}" }.join("\r\n"))
   end
 
   def build_status(request, match)
