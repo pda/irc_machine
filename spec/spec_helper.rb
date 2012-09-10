606ac7e1d3837af3596cc4abfd7d82a2d150a302
richo <richo@psych0tik.net>
2012-09-10 22:29:34 +1000

    Require monkeypatches for spec purposes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ Dir["#{File.expand_path(File.dirname(__FILE__))}/helpers/**/*.rb"].each do |f|
   require f
 end
 
+require File.expand_path("../../lib/irc_machine/monkey_patches.rb", __FILE__)
+
 RSpec.configure do |config|
   config.mock_framework = :mocha
 end
