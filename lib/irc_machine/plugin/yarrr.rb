56d499bc49cf6d0e5fb5388d453486975f131ee5
richo <richo@psych0tik.net>
2012-09-19 17:13:52 +1000

    Make regex simpler (and easier to trigger)

--- lib/irc_machine/plugin/yarrr.rb
+++ lib/irc_machine/plugin/yarrr.rb
@@ -1,6 +1,6 @@
 class IrcMachine::Plugin::Yarrr < IrcMachine::Plugin::Base
   def receive_line(line)
-    if line =~ /^:\S+ PRIVMSG (#+\S+) :[Yy][aA]+[rR]+(!+)?$/
+    if line =~ /^:\S+ PRIVMSG (#+\S+) :.*ya+r+/i
       `ssh saunamacmini ./yarrr.sh &`
     end
   end
