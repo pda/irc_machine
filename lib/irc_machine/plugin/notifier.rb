513d5bec63a53b379e7599b8064efd2acdce9c35
richo <richo@psych0tik.net>
2012-09-26 15:02:08 +1000

    Add a notifier plugin

---
+++ lib/irc_machine/plugin/notifier.rb
@@ -0,0 +1,35 @@
+# Your configuration should look something like:
+# - {
+#     "notication_name": "echo 'test'",
+#     "some_other_notification": [
+#       "Do this thing",
+#       "and also this thing"
+#     ]
+#   }
+class IrcMachine::Plugin::Notifier < IrcMachine::Plugin::Base
+  CONFIG_FILE = "notifier.json"
+
+  def notify(event)
+    case value = settings[event]
+    when Array
+      value.each do |n|
+        execute(n)
+      end
+    when String
+      execute(value)
+    end
+  end
+
+  def execute(command)
+    Kernel.system "#{command} &"
+  end
+
+  def settings
+    @settings ||= load_config
+  end
+
+  def load_config
+    JSON.load(open(File.expand_path(CONFIG_FILE)))
+  end
+
+end
