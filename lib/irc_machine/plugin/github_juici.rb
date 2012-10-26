34703fce2b1b44d1abed47762b19d49cca4003da
richo <richo@psych0tik.net>
2012-10-26 11:22:48 +1100

    Enable github status api in juici

--- lib/irc_machine/plugin/github_juici.rb
+++ lib/irc_machine/plugin/github_juici.rb
@@ -100,6 +100,19 @@ class IrcMachine::Plugin::GithubJuici < IrcMachine::Plugin::Base
       # and calling #drop_route!
       payload = ::IrcMachine::Models::JuiciNotification.new(request.body.read, :juici_url => juici_url)
       notify "#{payload.status} - #{project.name} :: #{commit.branch} :: built in #{time_elapsed.call}s :: JuiCI #{payload.url} :: PING #{commit.author_usernames.join(" ")}"
+      mark_build(commit, payload.status)
     }
+  end
+
+  def mark_build(commit, status)
+    project = "#{commit.repository.owner["name"]}/#{commit.repo_name}"
+    sha     = commit.after
+    status = case status
+             when "failed"
+               "failure"
+             else
+               status
+             end
+    plugin_send(:GithubCommitStatus, :mark, project, sha, status)
   end
 end
