e6d5ce786015125987040bffc323b0c3e03ee919
richo <richo@psych0tik.net>
2012-10-30 23:35:08 +1100

    Refactor to have less ugly nesting

--- lib/irc_machine/plugin/yarrr.rb
+++ lib/irc_machine/plugin/yarrr.rb
@@ -1,11 +1,11 @@
 class IrcMachine::Plugin::Yarrr < IrcMachine::Plugin::Base
   def receive_line(line)
     Time.now.tap do |today|
-      if today.month == 9 && today.day == 19
-        if line =~ /^:\S+ PRIVMSG (#+\S+) :.*ya+r+/i
-          plugin_send(:Notifier, :notify, "yarr")
-        end
-      end
+      return unless today.month == 9 && today.day == 19
+    end
+
+    if line =~ /^:\S+ PRIVMSG (#+\S+) :.*ya+r+/i
+      plugin_send(:Notifier, :notify, "yarr")
     end
   end
 end
