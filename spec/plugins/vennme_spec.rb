70c82ac89bc00fb2e5151f42e0500c5887a70639
richo <richo@psych0tik.net>
2012-12-17 12:14:54 +1100

    Add an end to end spec

--- spec/plugins/vennme_spec.rb
+++ spec/plugins/vennme_spec.rb
@@ -15,4 +15,10 @@ describe "Agent99::Plugin::VennMe" do
   it "Should return nil for invalid strings" do
     @plugin.parse("(rawr foo barr").should be_nil
   end
+
+  it "Should create correct urls" do
+    correct_url = "http://vennme.herokuapp.com/up?a=200&b=200&ab=110&alabel=rawr&ablabel=butts&blabel=foo"
+    @plugin.venn_me("(rawr (butts)   foo)").should == correct_url
+  end
+
 end
