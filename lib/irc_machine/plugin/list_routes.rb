f8eba4759e60a6df4be9ae536530209e5a7e68aa
Daniel Heath <daniel.r.heath@gmail.com>
2013-01-02 14:09:23 +1100

    Let the framework deal with the settings

--- lib/irc_machine/plugin/list_routes.rb
+++ lib/irc_machine/plugin/list_routes.rb
@@ -4,13 +4,12 @@ require 'net/http'
 class IrcMachine::Plugin::ListRoutes < IrcMachine::Plugin::Base
 
   def initialize(session)
-    @session = session
     super
     route :get, %r{^/$}, :list_routes
   end
 
   def list_routes(request, match)
-    ok @session.router.describe_patterns.map {|k, v| "#{k}: #{v.inspect}"}.join("\n")
+    ok session.router.describe_patterns.map {|k, v| "#{k}: #{v.inspect}"}.join("\n")
   end
 
 end
