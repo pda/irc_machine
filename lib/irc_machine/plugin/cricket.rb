3e93ce7727f4f8e82db5b227d3ea47b4236ecfd1
Michael Tibben <michael.tibben@99designs.com>
2013-01-03 15:29:03 +1100

    Adds cricket score plugin to agent99

---
+++ lib/irc_machine/plugin/cricket.rb
@@ -0,0 +1,37 @@
+require 'irc_machine'
+require 'open-uri'
+require 'nokogiri'
+
+class IrcMachine::Plugin::CricketScores < IrcMachine::Plugin::Base
+  attr_accessor :cricket_feed_url
+
+  def initialize
+    @cricket_feed_url = "http://synd.cricbuzz.com/j2me/1.0/livematches.xml"
+  end
+
+  def receive_line(line)
+    if line =~ /^:\S+ PRIVMSG (#+\S+) :#{session.state.nick}:?.*cricket score/
+      cricket_scores
+    end
+  end
+
+  def cricket_scores
+    cricket_xml = Nokogiri::XML(open(@cricket_feed_url))
+
+    cricket_match = cricket_xml.xpath("//match").first
+
+    cricket_str = [
+      sprintf('%s - %s',
+        cricket_match['mchDesc'],
+        cricket_match.xpath('//state').first['status']
+      ),
+      sprintf('%s %s/%s',
+        cricket_match.xpath('//mscr/btTm').first['sName'],
+        cricket_match.xpath('//mscr/btTm/Inngs').first['r'],
+        cricket_match.xpath('//mscr/btTm/Inngs').first['wkts'],
+      )
+    ].join("\n");
+
+    return cricket_str
+  end
+end
