271e8810efac51f8057d520556cd6db26b1bf3cd
richo <richo@psych0tik.net>
2013-03-03 20:39:18 +1100

    Add a ton of squirrels from @xzyfer

--- lib/irc_machine/plugin/jenkins_deploy.rb
+++ lib/irc_machine/plugin/jenkins_deploy.rb
@@ -102,6 +102,34 @@ class IrcMachine::Plugin::JenkinsNotify < IrcMachine::Plugin::Base
     http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/ShipIt2.jpg
     http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/ShipIt3.jpg
     http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/shipitship.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/00IU4s4.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/1L0TbhK.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/33e27I6.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/3426224093_d93a2c75b9_z.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/4145890585_db459aafd6.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/75xBpvk.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/AfTLsO8.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/H8uXLwS.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/HRY8DWi.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/HUBdnqf.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/L1pMLOU.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/O8gcXsl.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/PGAAIa3h.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/WCROsdb.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/fFQBdK4.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/fvCdM7n.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/goDCWg7.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/gvrXV5v.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/inCAP56.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/kcJKwq0.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/mKEZjDS.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/mRxaAK6.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/pECBUWJ.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/qKxZ80K.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/qNVpwT8.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/rDlRbkf.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/u17NYPg.jpg
+    http://99designs-shipit-squirrels.s3-website-us-east-1.amazonaws.com/xzyfer/uKI2cL3.jpg
   ]
 
 
