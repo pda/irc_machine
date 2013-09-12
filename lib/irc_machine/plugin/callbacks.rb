82fff46968927d21187e1f3de8a6fac56fc19fed
Richo Healey <richo@psych0tik.net>
2013-09-12 14:19:41 +1000

    Allow routing to a per method callback, as well as a method global one

--- lib/irc_machine/plugin/callbacks.rb
+++ lib/irc_machine/plugin/callbacks.rb
@@ -4,7 +4,8 @@ module Callbacks
 
   def has_callbacks(path, opts={}, &block)
     name = opts[:method_name] || raise("Method name must be specified.")
-    define_method name do
+    define_method name do |&blk|
+      blk ||= (block || raise("No block given"))
       callback_base = opts[:callback_base] || settings["callback_base"]
 
       callback = {}
@@ -12,7 +13,7 @@ module Callbacks
         callback[:path] = "#{path}/#{UUID.new.generate}"
         uri.path = callback[:path]
       end
-      route(:post, callback[:path], block)
+      route(:post, callback[:path], blk)
       callback
     end
  	end
