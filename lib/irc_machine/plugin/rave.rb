1a7284b578ef05fe16cb39f4c2af881ae524ce49
Richo Healey <richo@psych0tik.net>
2013-09-24 15:09:13 +1000

    Send the gif to the channel instead

--- lib/irc_machine/plugin/rave.rb
+++ lib/irc_machine/plugin/rave.rb
@@ -4,7 +4,7 @@ class IrcMachine::Plugin::Rave < IrcMachine::Plugin::Base
 
   def receive_line(line)
     if line =~ /^:(\S+)!\S+ PRIVMSG (#+\S+) :#{session.state.nick}:? transformers rave$/
-      session.msg $1, "http://i.imgur.com/lHgEm.gif"
+      session.msg $2, "http://i.imgur.com/lHgEm.gif"
     end
   end
 
